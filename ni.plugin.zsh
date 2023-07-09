04a8417933936f26168756252022eba81fbeed45
Denis Moiseev <isdenmois@gmail.com>
2023-07-09 15:48:16 +0300

    chore: zsh completions

--- ni.plugin.zsh
+++ ni.plugin.zsh
@@ -5,44 +5,27 @@ GITHUB_REPO="isdenmois/ni"
 [[ -z "$NI_HOME" ]] && export NI_HOME="$HOME/.local/share/ni"
 
 _zsh_ni_install() {
-    [[ ! -f "$NI_HOME" ]] && mkdir -p "$NI_HOME"
-    curl -s https://api.github.com/repos/$GITHUB_REPO/releases/latest \
-        | grep browser_download_url \
-        | head -1 \
-        | cut -d : -f 2,3 \
-        | tr -d \" \
-        | xargs -n1 curl -sL -o $NI_HOME/ni.tar.gz
-    tar -xf $NI_HOME/ni.tar.gz -C $NI_HOME
-    rm $NI_HOME/ni.tar.gz
-    chmod +x $NI_HOME/*
+  [[ ! -f "$NI_HOME" ]] && mkdir -p "$NI_HOME"
+  curl -s https://api.github.com/repos/$GITHUB_REPO/releases/latest \
+    | grep browser_download_url \
+    | head -1 \
+    | cut -d : -f 2,3 \
+    | tr -d \" \
+    | xargs -n1 curl -sL -o $NI_HOME/ni.tar.gz
+  tar -xf $NI_HOME/ni.tar.gz -C $NI_HOME
+  rm $NI_HOME/ni.tar.gz
+  chmod +x $NI_HOME/*
 }
 
 _zsh_ni_load() {
-    export PATH="$NI_HOME:$PATH"
+  export PATH="$NI_HOME:$PATH"
 }
 
 ni-update(){
-    echo "Download a ni update..."
-    rm -rf $NI_HOME
-    _zsh_ni_install
-    echo "Ni has been updated"
-}
-
-_nr() {
-  _arguments -C '*:: :->verbs'
-
-  if (( CURRENT == 1 )); then
-    compadd `ni-completions -s` 
-    return
-  fi
-
-  parts=("${(@f)$(ni-completions -a $words[1])}")
-  for i ("$parts[@]") _arguments $i
-  return
-}
-
-_nun() {
-  compadd `ni-completions -d`
+  echo "Download a ni update..."
+  rm -rf $NI_HOME
+  _zsh_ni_install
+  echo "Ni has been updated"
 }
 
 # install ni if it isnt already installed
@@ -50,13 +33,9 @@ _nun() {
 
 # load ni if it is installed
 if [[ -f "$NI_HOME/ni" ]]; then
-    _zsh_ni_load
+  _zsh_ni_load
 fi
 
 # Aliases
 alias nx=npx
 alias nid="ni -D"
- 
- # Completions
-compdef _nr nr
-compdef _nun nun
